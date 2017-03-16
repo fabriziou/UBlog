d67f6201c7fe324128d9966289a9eb757e965ac0
Teddy Thioulouse <teddy.thioulouse@outlook.com>
2017-03-16 12:19:45 -0400

    Add route /login

--- src/router.py
+++ src/router.py
@@ -4,4 +4,5 @@ from webapp2 import Route
 app = WSGIApplication([
     Route(r'/', handler='app.main.MainPage', name='home'),
     Route(r'/signup', handler='app.signup.SignupPage', name='signup'),
+    Route(r'/login', handler='app.login.LoginPage', name='login'),
 ], debug=True)
