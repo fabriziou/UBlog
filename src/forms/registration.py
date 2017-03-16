0813b1d5437c3cdc7c661261e721a421bb65dc0d
Teddy Thioulouse <teddy.thioulouse@outlook.com>
2017-03-16 12:08:13 -0400

    Add EmailIsUnique validator

--- src/forms/registration.py
+++ src/forms/registration.py
@@ -1,6 +1,6 @@
 from wtforms import Form, StringField, PasswordField
 from wtforms.validators import InputRequired, Email, Length, EqualTo, Regexp
-from validators.Users import UsernameIsUnique
+from validators.Users import UsernameIsUnique, EmailIsUnique
 
 
 class RegistrationForm(Form):
@@ -9,7 +9,7 @@ class RegistrationForm(Form):
                                         Regexp(regex=r"^[a-zA-Z]{3,24}" +
                                         r"(?:\s[a-zA-Z]+)*$"),
                                         UsernameIsUnique()])
-    email = StringField('Email', [InputRequired(), Email()])
+    email = StringField('Email', [InputRequired(), Email(), EmailIsUnique()])
     password = PasswordField('Password', [InputRequired(),
                                           EqualTo('confirm',
                                           message='Passwords must match'),
--- src/validators/Users.py
+++ src/validators/Users.py
@@ -9,6 +9,18 @@ class UsernameIsUnique(object):
         self.message = message
 
     def __call__(self, form, field):
-        # Return a error if a user with the same username is found
+        # Return a error if the username is found in Users
         if Users.all(keys_only=True).filter("username", field.data).get():
+            raise ValidationError(self.message)
+
+
+class EmailIsUnique(object):
+    def __init__(self, message=None):
+        if not message:
+            message = u'This email is already used'
+        self.message = message
+
+    def __call__(self, form, field):
+        # Return a error if the email is found in Users
+        if Users.all(keys_only=True).filter("email", field.data).get():
             raise ValidationError(self.message)
