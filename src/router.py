f0a95b36520924328c9894cb3bef591b6446b4e9
Teddy Thioulouse <teddy.thioulouse@outlook.com>
2017-03-17 11:30:51 -0400

    Add AddPostPage

--- src/router.py
+++ src/router.py
@@ -2,8 +2,14 @@ from webapp2 import WSGIApplication
 from webapp2 import Route
 
 app = WSGIApplication([
+    # Home
     Route(r'/', handler='app.main.MainPage', name='home'),
+
+    # Logout pages
     Route(r'/signup', handler='app.signup.SignupPage', name='signup'),
     Route(r'/login', handler='app.login.LoginPage', name='login'),
+
+    # Login pages
     Route(r'/logout', handler='app.logout.LogoutPage', name='logout'),
+    Route(r'/posts/add', handler='app.posts.AddPostPage', name='addpost'),
 ], debug=True)
