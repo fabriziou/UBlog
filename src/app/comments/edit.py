fd20df846268aa1c8f32a1cb41c7cda4884b15e1
Teddy Thioulouse <teddy.thioulouse@outlook.com>
2017-03-18 20:01:46 -0400

    Add POST method for EditComment

--- src/app/comments/edit.py
+++ src/app/comments/edit.py
@@ -19,6 +19,26 @@ class EditCommentPage(Handler):
         form = CommentForm(obj=comment)
         self.render_editcomment(form=form, post=post, comments=old_comments)
 
+    def post(self, post_key, comment_key):
+        """ Display the form to edit a comment
+
+            :param post_key:
+                Key of the post
+            :param comment_key:
+                Key of the comment
+        """
+        post = Post.get(post_key)
+        comment = Comment.get(comment_key)
+        old_comments = Comment.get_all_by_post(post=post, before_date=comment.creation_date)
+        form = CommentForm(self.request.POST)
+        if form.validate():
+            comment_key = Comment.update_comment(comment, form.content.data)
+            if comment_key:
+                self.redirect_to("viewpost", post_key=post_key,
+                                 _fragment=str(comment_key))
+
+        self.render_editcomment(form=form, post=post, comments=old_comments)
+
     def render_editcomment(self, form, post, comments):
         """Include the form in a template and render it
 
